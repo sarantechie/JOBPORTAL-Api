882b9e1ecdb1c1887228bdb006312964438ffefd
sarantechie <saran.tech25@gmail.com>
2025-03-26 15:20:08 +0530

    added mongo uri in versel json

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      connectTimeoutMS: 60000,
+      socketTimeoutMS: 60000,
+    });
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error.message);
